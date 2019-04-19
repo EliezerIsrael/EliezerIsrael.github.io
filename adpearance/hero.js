81f5b6f66a3a2abc3281a55d9dfbe97cbbf6c7c6
Lev Israel <eliezer.israel@gmail.com>
2019-04-19 14:05:51 -0400

    Documentation.  Pull out configurable variables.

--- adpearance/hero.js
+++ adpearance/hero.js
@@ -1,13 +1,23 @@
 
-const h = 500;
-let w = window.innerWidth;  // Remove in favor of a call to redraw?
+///////////////////////////////////
+// Easily configurable variables //
+///////////////////////////////////
 
-const transition_duration = 1500;
-const pause = 6500;
+const h = 700;                        // (px) Height of visualization
+const graph_top = 2/3;                // (0..1)  How far down from the top line is the area graph?
+const initial_area_delay = 1000;      // (ms) Delay before first animation of area graph
+const pause = 6500;                   // (ms) Time between transitions of area graph
+const initial_bubble_delay = 4000;    // (ms) Delay before bubbles fade in
+const initial_bubble_duration = 8000; // (ms) Length of initial bubble fade in
+const transition_duration = 1500;     // (ms) Duration of each active transition of area graph
+const background_color = "#126bcd";   // Background color
 
-const background_color = "#126bcd";
 
-// Data Points for area graph
+
+/////////////////////////
+// Data for area graph //
+/////////////////////////
+
 // Array of objects.  Each object is one state of graph.
 // Each object has keys "title" and "values" - an array of numbers
 
@@ -21,65 +31,85 @@ const data = [{title: "website visits", values: [7060309, 7324365, 7656326, 6390
     {title: "dynamic emails sent", values: [173243, 193458, 205296, 216979, 198229, 202007, 215451]},
     {title: "inventory changes monitored", values: [520082, 469083, 480548, 786334, 567338, 639586, 884198]}];
 
+/////////////////////
+// Data for people //
+/////////////////////
+
+// Array of objects.  Each object is one person.
+// Configurable Attributes of each object:
+// - text       Array of two strings
+// - x_factor   At what fraction of visualization's width this circle renders (Left: 0, Right: 1)
+// - y_factor   At what fraction of visualization's height this circle renders (Top : 0, Bottom : 1)
+// - img        The picture used for this person
+
+// All objects get decorated with numbers that control its trajectory, fade, etc.  Documented below.
 
 const people = [
     {
         text: ["What type of car", "is Liz looking for?"],
         x_factor: .10,
-        y: h * .55,
+        y_factor: .55,
         img: "img/brunette_woman_png.png"
     },
     {
         text: ["Is Malcolm logged","in our CRM?"],
         x_factor: .15,
-        y: h * .1,
+        y_factor: .1,
         img: "img/bald_man_png.png"
     },
     {
         text: ["What did Hannah", "search for last month?"],
         x_factor: .62,
-        y: h * .17,
+        y_factor: .17,
         img: "img/shorthaired_woman_png.png"
     },
     {
         text: ["Did we return","Michael's call?"],
         x_factor: .9,
-        y: h * .5,
+        y_factor: .5,
         img: "img/tan_man_png.png"
     },
     {
         text: ["What price range is Gina","searching within?"],
         x_factor: .4,
-        y: h * .35,
+        y_factor: .35,
         img: "img/older_brunette_woman_png.png"
     },
     {
         text: ["What marketing channels are","most profitable this month?"],
         x_factor: .75,
-        y: h * .1,
+        y_factor: .1,
         img: "img/glasses_man_png.png"
     },
     {
         text: ["How do our sales compare","to others in the area?"],
         x_factor: .90,
-        y: h * .25,
+        y_factor: .25,
         img: "img/greyhaired_man_png.png"
     }
 ].map(o => {
-    o.t = Math.random() * 2 * Math.PI;
-    o.dt = Math.random() * .1 - .05;
-    o.o = Math.random() * 2 * Math.PI; // opacity seed
-    o.dx = 0;
-    o.dy = 0;
+    o.t = Math.random() * 2 * Math.PI;  // Initial trajectory
+    o.o = Math.random() * 2 * Math.PI;  // Initial opacity
+    o.dt = Math.random() * .1 - .05;    // Change in trajectory & fade at each step
+    o.dx = 0;   // Accumulated change in X from start
+    o.dy = 0;   // Accumulated change in Y from start
     return o;
 });
 
-////  Canvas  ////
+/////////////////////
+//      Canvas     //
+/////////////////////
+
+let w = window.innerWidth;
 const svg = d3.select("body").append("svg").attr("width",w).attr("height",h);
 const background = svg.append("rect").attr("width", w).attr("height", h).attr("fill", background_color);
-
-////  Data Manipulation  ////
 let current_graph = -1;
+
+
+/////////////////////////////////////
+////  Data Processing Functions  ////
+/////////////////////////////////////
+
 const number_of_points = () => data[(current_graph === -1)?0:current_graph].values.length;
 const values = () => (current_graph === -1) ? new Array(number_of_points()).fill(0) : data[current_graph]["values"];
 const points = () => [values().map((y, i) => [i, y])];
@@ -92,7 +122,7 @@ const x = d3.scaleLinear()
 
 const y = d3.scaleLinear()
     .domain([d3.min(values()) * .85, d3.max(values()) + 1])
-    .range([h, 2 * h / 3]);
+    .range([h, h * graph_top]);
 
 const area = d3.area()
     .curve(d3.curveCardinal)
@@ -100,8 +130,10 @@ const area = d3.area()
     .y0(y(0))
     .y1(d => y(d[1]));
 
-
+//////////////////////////
 ////  Initial Render  ////
+//////////////////////////
+
 const path = svg.selectAll("path")
     .data(points)
     .enter()
@@ -154,13 +186,13 @@ person_bubble_group
     .append("image")
     .attr("xlink:href", d => d.img)
     .attr("x", d => (w * d.x_factor) - 28)
-    .attr("y", d => d.y - 28)
+    .attr("y", d => (h * d.y_factor) - 28)
     .attr("width", 56)
     .attr("height", 56);
 person_bubble_group
     .append("circle")
     .attr("cx", d => (w * d.x_factor))
-    .attr("cy", d => d.y)
+    .attr("cy", d => (h * d.y_factor))
     .attr("r", "27")
     .attr("fill", "none")
     .attr("stroke", "#4e8fdb")
@@ -169,7 +201,7 @@ person_bubble_group
 person_bubble_group
     .append("circle")
     .attr("cx", d => (w * d.x_factor))
-    .attr("cy", d => d.y)
+    .attr("cy", d => (h * d.y_factor))
     .attr("r", "37")
     .attr("fill", "none")
     .attr("stroke", "#206ece")
@@ -180,7 +212,7 @@ person_bubble_group
     .attr("class", "question")
     .attr("text-anchor", "middle")
     .attr("x", d => (w * d.x_factor))
-    .attr("y", d => d.y + 40)
+    .attr("y", d => (h * d.y_factor) + 40)
     .text(d => d.text[0])
     .attr("font-family", "sans-serif")
     .attr("font-size", "12px")
@@ -190,14 +222,17 @@ person_bubble_group
     .attr("class", "question")
     .attr("text-anchor", "middle")
     .attr("x", d => (w * d.x_factor))
-    .attr("y", d => d.y + 60)
+    .attr("y", d => (h * d.y_factor) + 60)
     .text(d => d.text[1])
     .attr("font-family", "sans-serif")
     .attr("font-size", "12px")
     .attr("fill", "#5286e3");
 
+/////////////////////
 ////  Animation  ////
-const unit = .1;
+/////////////////////
+
+const unit = .1;        // How far each bubble moves, at each clock tick
 normalize_opacity  = d => Math.sin(d) / 4 + .75;
 function bubble_tick() {
     people.forEach(person => {
@@ -260,16 +295,18 @@ function area_tick() {
 
 }
 
-setTimeout(area_tick, 1000);  // Initial transition of area graph
-d3.interval(area_tick, pause);  // All subsequent transitions of area graph
+// The process of all animations is controlled here.
+
+setTimeout(area_tick, initial_area_delay);  // Delay before initial transition of area graph
+d3.interval(area_tick, pause);              // All subsequent transitions of area graph
 d3.interval(bubble_tick, 66);
 svg.selectAll("g.person")
     .data(people)
     .transition()
-    .delay(4000)
-    .duration(8000)
+    .delay(initial_bubble_delay)
+    .duration(initial_bubble_duration)
     .attr("opacity", d => normalize_opacity(d.o));
-setTimeout(function(){d3.interval(bubble_opacity_tick, 66);}, 12000);
+setTimeout(function(){d3.interval(bubble_opacity_tick, 66);}, initial_bubble_delay + initial_bubble_duration);
 
 // Redraw when browser window resized.
 window.addEventListener("resize", redraw);
